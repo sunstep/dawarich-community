a275f26d89f80d5dae44920255b38708ee9ef5b3
sunstep <abrahimismail2004+github@gmail.com>
2026-09-05 19:59:04 +0200

    Add missing files

---
+++ android/app/src/main/kotlin/dev/abralabs/travel/MainActivity.kt
@@ -0,0 +1,14 @@
+package dev.abralabs.travel
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterFragmentActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        SystemSettingsChannel.register(flutterEngine, this)
+        BuildConfigChannel.register(flutterEngine)
+    }
+}
